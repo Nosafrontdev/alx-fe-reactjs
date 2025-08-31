436e2e99bc7e0ce7f8fb77b921fdbc9cb4dda046
Nosafrontdev <nosyblink@gmail.com>
2025-08-31 21:10:18 +0100

    m

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './Header';
 import Footer from './Footer';
 import MainContent from './MainContent';
