3da26c51593e050251e7439364aa5bae47098767
Nosafrontdev <nosyblink@gmail.com>
2025-09-07 21:23:47 +0100

    working website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <p>© 2023 My Company. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
