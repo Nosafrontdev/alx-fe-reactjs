c8c0eddad75e0a18e8547068133bbce8a07ca80c
Nosafrontdev <nosyblink@gmail.com>
2025-09-14 16:09:45 +0100

    omo

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <Routes>
-      {/* Home route */}
-      <Route
-        path="/"
-        element={
-          <>
-            <h1>Recipe App</h1>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        }
-      />
-
-      {/* Recipe details route */}
-      <Route path="/recipes/:id" element={<RecipeDetails />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Recipe App</h1>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
