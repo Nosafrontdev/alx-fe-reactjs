4c6ddd5ca25fba54834b9f5bb475d96a87ff9836
Nosafrontdev <nosyblink@gmail.com>
2025-09-15 09:52:14 +0100

    omo

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,6 +1,5 @@
-// EditRecipeForm.jsx
 import { useState } from "react";
-import  useRecipeStore  from "./recipeStore";
+import useRecipeStore from "./recipeStore"; // <-- fixed import
 
 const EditRecipeForm = ({ recipe }) => {
   const updateRecipe = useRecipeStore((state) => state.updateRecipe);
