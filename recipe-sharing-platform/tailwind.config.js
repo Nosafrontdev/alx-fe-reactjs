53dd856b77feb88fdf897957a2426df812c8f6e5
NOSA <nosyblink@gmail.com>
2025-10-04 14:55:32 +0100

    Delete recipe-sharing-platform directory

--- recipe-sharing-platform/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
