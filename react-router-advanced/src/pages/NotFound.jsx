9345549aa931bd6644f28e4d5d7afd5c5161e696
Nosafrontdev <nosyblink@gmail.com>
2025-10-12 21:27:06 +0100

    omo

---
+++ react-router-advanced/src/pages/NotFound.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function NotFound() {
+  return <h2>404 — Page Not Found</h2>;
+}
---
+++ react-router-advanced/src/pages/ProfileSettings.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function ProfileSettings() {
+  return <p>Manage your account settings here.</p>;
+}
